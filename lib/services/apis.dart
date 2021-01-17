531de7f9357975ba7bca8bd3137c32c633671a97
Shrinandan N <19442106+Shrinandan-N@users.noreply.github.com>
2021-01-17 13:48:32 -0800

    Update apis.dart

--- lib/services/apis.dart
+++ lib/services/apis.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class APIS {
   Future<Map<dynamic, dynamic>> getWeatherData(String lat, String long) async {
-    String apiKey = "c5e06a3001f70b0df68c3203d15c680f";
+    String apiKey = "API KEY";
     String url =
         "https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=${apiKey}";
     var jsonResponse;
